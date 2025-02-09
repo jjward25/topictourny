510d3b44f32da3a4117ad538f31e7f331b91a359
jjward25 <45015765+jjward25@users.noreply.github.com>
2025-02-09 17:28:30 -0500

    unused6

--- components/random-game.tsx
+++ components/random-game.tsx
@@ -5,7 +5,6 @@ import Link from "next/link"
 import Image from "next/image"
 import { games } from "@/data/bracket_list"
 
-type Matchup = [string, string]
 type GameCategory = {
   [key: string]: string[]
 }
