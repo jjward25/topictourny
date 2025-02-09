d9a2e9377ed8f2eeae4793c2a508ecb60611298d
jjward25 <45015765+jjward25@users.noreply.github.com>
2025-02-09 17:26:43 -0500

    type5

--- components/random-game.tsx
+++ components/random-game.tsx
@@ -6,7 +6,6 @@ import Image from "next/image"
 import { games } from "@/data/bracket_list"
 
 type Matchup = [string, string]
-type Round = Matchup[]
 type GameCategory = {
   [key: string]: string[]
 }
