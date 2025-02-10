3e73b875862804d3493f68e8103ebecda04e3c59
jjward25 <45015765+jjward25@users.noreply.github.com>
2025-02-10 09:08:17 -0500

    newGames

--- data/bracket_list.ts
+++ data/bracket_list.ts
@@ -1,185 +1,389 @@
 export const games = [
+  {
+    "Album of the Year Winner": [
+      "Taylor Swift",
+      "Frank Sinatra",
+      "OutKast",
+      "Michael Jackson",
+      "Billy Joel",
+      "Stevie Wonder",
+      "Fleetwood Mac",
+      "Lauryn Hill"
+    ]
+  },
+
+  {
+    "Apartment Amenity": [
+      "An Average Roommate",
+      "Washer/Dryer",
+      "Quiet",
+      "Building Elevator",
+      "New Appliances",
+      "Recycling",
+      "Closet Space",
+      "Sunlight"
+    ]
+  },
+
+  {
+    "Breakfast": [
+      "Bacon Egg Cheese",
+      "Barrys Bootcamp",
+      "Black Coffee",
+      "Banana / Fruits",
+      "Granola",
+      "Homestyle",
+      "Waffles or Pancakes",
+      "Huevos Rancheros"
+    ]
+  },
+
+  {
+    "Cuisines": [
+      "Italian",
+      "Chinese",
+      "Thai",
+      "French",
+      "Indian",
+      "Ethiopian",
+      "Mexican",
+      "Halal"
+    ]
+  },
+
+  {
+    "Fashion": [
+      "Sun Dresses",
+      "Grey Sweatpants",
+      "Yogapants",
+      "Flannels",
+      "The Met Gala",
+      "Frosted Tips",
+      "Pockets",
+      "Crocs"
+    ]
+  },
+
+
+  {
+    "Joe's": [
+      "Biden",
+      "Jonas",
+      "Mama",
+      "DiMaggio",
+      "Pesci",
+      "Stalin",
+      "of Nazareth",
+      "Rogan"
+    ]
+  },
+
+  {
+    "Mall Stores": [
+      "Sears",
+      "Spencer's",
+      "Pacsun",
+      "Auntie Anne's",
+      "The Booths in the Middle",
+      "Hot Topic",
+      "Massage Chairs",
+      "Halloween Pop Ups"
+    ]
+  },
+  {
+    "Vacations": [
+      "Road Trip",
+      "Anywhere New",
+      "Resort, Cruise, All-Inclusive",
+      "Camping or Hiking",
+      "Staycation",
+      "Lake House",
+      "Business Trip",
+      "Beach"
+    ]
+  },
+
+  {
+    "TV Channels": [
+      "90s MTV",
+      "HGTV",
+      "History Channel",
+      "Discovery Channel",
+      "PBS",
+      "Animal Planet",
+      "AMC",
+      "Food Network"
+    ]
+  },
+   
     {
-      "Album of the Year Winner": [
-        "Taylor Swift",
-        "Frank Sinatra",
-        "OutKast",
-        "Michael Jackson",
-        "Billy Joel",
-        "Stevie Wonder",
-        "Fleetwood Mac",
-        "Lauryn Hill"
+      "First Dates": [
+        "Park",
+        "Family Dinner",
+        "Double-date",
+        "Facetime",
+        "Errands",
+        "Free",
+        "Escape Room",
+        "Coffee Shop"
       ]
     },
-  
+ 
     {
-      "Apartment Amenity": [
-        "An Average Roommate",
-        "Washer/Dryer",
-        "Quiet",
-        "Building Elevator",
-        "New Appliances",
-        "Recycling",
-        "Closet Space",
-        "Sunlight"
+      "Drunk Personalities": [
+        "Emotional Confessor",
+        "Instagram Story Spammer",
+        "Fight Picker",
+        "Spontaneous Dancer",
+        "Amateur Philosopher",
+        "Disappeared Without Warning",
+        "Needs McDonald's NOW",
+        "Suddenly Bilingual"
       ]
     },
-  
     {
-      "Art Form": [
-        "Poems",
-        "Paintings",
-        "Drawings",
-        "Photographs",
-        "Movies",
-        "Books",
-        "Theater",
-        "Stand Up"
+      "Side Hustles": [
+        "Crypto Trading",
+        "Dropshipping",
+        "Feet Pics",
+        "Wellness Coaching",
+        "Podcast About Nothing",
+        "Etsy Shop",
+        "Real Estate",
+        "Influencing"
       ]
     },
-  
     {
-      "Breakfast": [
-        "Bacon Egg Cheese",
-        "Barrys Bootcamp",
-        "Black Coffee",
-        "Banana / Fruits",
-        "Granola",
-        "Homestyle",
-        "Waffles or Pancakes",
-        "Huevos Rancheros"
+      "Hangover Cures": [
+        "Greasy Breakfast",
+        "Hair of the Dog",
+        "Gatorade",
+        "Sleep All Day",
+        "Cold Shower",
+        "Advil and Prayer",
+        "Pedialyte",
+        "Crying"
       ]
     },
-  
     {
-      "Cuisines": [
-        "Italian",
-        "Chinese",
-        "Thai",
-        "French",
-        "Indian",
-        "Ethiopian",
-        "Mexican",
-        "Halal"
+      "Internet Personalities": [
+        "Instagram Models",
+        "Redditors",
+        "Twitter Warriors",
+        "LinkedIn Gurus",
+        "TikTok Dancers",
+        "YouTube Pranksters",
+        "Twitch Streamers",
+        "NFT Bros"
       ]
     },
-  
+
     {
-      "Fashion": [
-        "Sun Dresses",
-        "Grey Sweatpants",
-        "Yogapants",
-        "Flannels",
-        "The Met Gala",
-        "Frosted Tips",
-        "Pockets",
-        "Crocs"
+      "Pandemic Habits": [
+        "Sourdough Making",
+        "Zoom Happy Hours",
+        "Day Drinking",
+        "TikTok Dancing",
+        "Online Shopping",
+        "Not Wearing Pants",
+        "Plant Parenting",
+        "Netflix Binging"
       ]
     },
-  
-    {
-      "Game Night Games": [
-        "Monopoly",
-        "Kings",
-        "Scattergories",
-        "Life",
-        "Cards Against Humanity",
-        "Charades",
-        "Twister",
-        "Jackbox TV"
-      ]
-    },
-  
-    {
-      "Joe's": [
-        "Biden",
-        "Jonas",
-        "Mama",
-        "DiMaggio",
-        "Pesci",
-        "Stalin",
-        "of Nazareth",
-        "Rogan"
-      ]
-    },
-  
-    {
-      "Karaoke Music": [
-        "Britney Spears",
-        "80s",
-        "Rap",
-        "Michael Jackson",
-        "Abba",
-        "90s",
-        "John Mayer",
-        "I'm afraid to order at Subway"
-      ]
-    },
-  
-    {
-      "Mall Stores": [
-        "Sears",
-        "Spencer's",
-        "Pacsun",
-        "Auntie Anne's",
-        "The Booths in the Middle",
-        "Hot Topic",
-        "Massage Chairs",
-        "Halloween Pop Ups"
-      ]
-    },
-  
-    {
-      "Sports": [
-        "NASCAR",
-        "Rollerderby",
-        "Competitive Eating",
-        "Track and Field",
-        "Chess",
-        "Ping Pong",
-        "Fencing",
-        "E-Sports"
-      ]
-    },
-  
-    {
-      "Rock Structure": [
-        "Mount Rushmore",
-        "The Great Pyramids",
-        "The Grand Canyon",
-        "The Great Wall of China",
-        "Mount Everest",
-        "Volcanos",
-        "The Galapagos Islands",
-        "Taj Mahal"
-      ]
-    },
-  
-    {
-      "Types of Travel": [
-        "Road Trip",
-        "Cruise",
-        "All-Inclusive",
-        "Camping",
-        "Staycation",
-        "Sailing",
-        "Business Trip",
-        "School Trip"
-      ]
-    },
-  
-    {
-      "TV Channel": [
-        "90s MTV",
-        "HGTV",
-        "History Channel",
-        "Discovery Channel",
-        "PBS",
-        "Animal Planet",
-        "AMC",
-        "Food Network"
-      ]
-    }
+      {
+        "90s Movie Characters": [
+          "Tyler Durden",
+          "Hannibal Lecter",
+          "Alicia Silverstone in Clueless",
+          "The T-Rex from Jurassic Park",
+          "Rose from Titanic",
+          "Agent Smith",
+          "Mrs. Doubtfire",
+          "Patrick Bateman"
+        ]
+      },
+      {
+        "Celebrity Downfalls": [
+          "Will Smith's Slap",
+          "Kanye's Twitter",
+          "Johnny Depp's Trial",
+          "Kevin Spacey's Career",
+          "GameStop Stock Bros",
+          "Elon's X Rebrand",
+          "NFT Collapse",
+          "Meta's Metaverse"
+        ]
+      },
+      {
+        "Famous Flops": [
+          "Google Glass",
+          "Quibi",
+          "Crystal Pepsi",
+          "HD DVD",
+          "Windows Vista",
+          "Segway",
+          "3D TVs",
+          "Amazon Fire Phone"
+        ]
+      },
+      {
+        "Netflix True Crime": [
+          "Tiger King",
+          "Tinder Swindler",
+          "FYRE Festival",
+          "Don't F**k with Cats",
+          "Making a Murderer",
+          "The Puppet Master",
+          "Bad Vegan",
+          "LuLaRich"
+        ]
+      },
+      {
+        "TV Show Deaths": [
+          "Game of Thrones Red Wedding",
+          "Breaking Bad Finale",
+          "Lost Smoke Monster",
+          "Walking Dead Glenn",
+          "Sopranos Cut to Black",
+          "Dexter's Sister",
+          "Grey's Anatomy Anyone",
+          "Better Call Saul Ending"
+        ]
+      },
+      {
+        "Viral Moments": [
+          "Bernie's Mittens",
+          "Ship Stuck in Canal",
+          "Area 51 Raid",
+          "Gorilla Glue Girl",
+          "Ice Bucket Challenge",
+          "Storm Area 51",
+          "Kim K Crying Face",
+          "Josh Fight"
+        ]
+      },
+      {
+        "Y2K Nostalgia": [
+          "Razor Scooters",
+          "AOL Instant Messenger",
+          "Tamagotchi",
+          "Limewire",
+          "Style",
+          "TV",
+          "Cell Phones with Snake",
+          "Myspace Top 8"
+        ]
+      },
+      {
+        "Adult Achievement Awards": [
+          "Has Savings Account",
+          "Therapy Regular",
+          "Has Real Estate",
+          "Sleeps Eight Hours",
+          "Good Style",
+          "Dentist Appointment Made",
+          "Plants Still Alive",
+          "Inbox Zero"
+        ]
+      },
+      {
+        "Death Row Meals": [
+          "Single Olive (Victor Feguer)",
+          "KFC Bucket (John Wayne Gacy)",
+          "Just Coffee (Timothy McVeigh)",
+          "Two Pints Ice Cream (Gary Gilmore)",
+          "Justice, Equality, World Peace (Philip Workman)",
+          "Dirt from Jerusalem (Aileen Wuornos)",
+          "24 Tacos & Media Interview (Lawrence Brewer)",
+          "Birthday Cake (Ricky Ray Rector)"
+        ]
+      },
+      {
+        "Relationship Stages": [
+          "First Sleepover",
+          "Met The Friends",
+          "Left A Toothbrush",
+          "Said I Love You",
+          "Met The Parents",
+          "Moved In Together",
+          "Got A Pet Together",
+          "Joint Bank Account"
+        ]
+      },
+        {
+          "Capitalism Core": [
+            "Girl Boss (Derogatory)",
+            "Literally Working Until Death",
+            "Rent > Entire Paycheck",
+            "LinkedIn Trauma Posting",
+            "Gaslight Gatekeep Grindset",
+            "Unpaid Experience Required",
+            "Quiet Quitting",
+            "Side Hustle / Polyworking (Surviving)"
+          ]
+        },
+
+        {
+          "Dying Inside But": [
+            "✨Mentally Ill and Thriving✨",
+            "This Is Fine (Not Fine)",
+            "Main Character Energy (Dissociating)",
+            "Living Laugh Loving My Trauma",
+            "Romanticizing The Void",
+            "Having A Normal One :)",
+            "Dead Inside But Make It Fashion",
+            "Vibing With The Abyss"
+          ]
+        },
+        {
+          "Failed Talking Stage": [
+            "Trauma Dumped Too Soon",
+            "Red Flag Collection",
+            "Ghosted (Already Dead)",
+            "Parasocial But Make It Dating",
+            "Touch Starved Era",
+            "Dating App Speedrun",
+            "Attachment Style: Cursed",
+            "Pandemic Season 5"
+          ]
+        },
+        {
+          "Millennial vs Gen Z": [
+            "Harry Potter House",
+            "Pizza Is Not A Personality",
+            "Side Part Slander",
+            "Skinny Jeans Burial",
+            "Living Laugh Leaving",
+            "Adulting (Derogatory)",
+            "Not The Avocado Toast",
+            "Doggo Speech Cemetery"
+          ]
+        },
+        {
+          "No Thoughts Just": [
+            "Intrusive Thoughts Winning",
+            "Brain Empty Error 404",
+            "Bestie We're Not Okay",
+            "Undiagnosed Everything",
+            "Chronic Online Disease",
+            "Going Feral Actually",
+            "Functioning't",
+            "Weaponized Incompetence"
+          ]
+        },
+        {
+          "Vibe Check Failed": [
+            "Replied K", 
+            "Made Eye Contact While Eating a Banana", 
+            "Spotify Wrapped Was Embarrassing", 
+            "Pheromones",
+            "Moaned While Stretching",
+            "Sent a ‘U Up?’ Text at 3 PM",
+            "No Snapstreaks",
+            "Called Daddy in the Wrong Context"
+          ]
+
+        }
   ]
   
   
